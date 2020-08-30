c041cd8d6cf6e28cf2a48475d99e4da435a22f66
Samba Ndiaye <ext_ndiaye76@orange-sonatel.com>
2020-08-30 00:07:48 +0000

    Added welcome message

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 require('dotenv').config()
+const messages = require('./messages.json')
 const discord = require('discord.js')
-const client = new discord.Client()
 
+const client = new discord.Client()
 client.on('ready',function(){
   process.stdout.write('Bot is up and running....')
 })
+
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
+  if (!channel) return;
+  channel.send(`${messages.welcomeMessage} ${member} ! ${messages.welcomeMessageBody}`);
+});
 
 client.login(process.env.BOT_TOKEN)
